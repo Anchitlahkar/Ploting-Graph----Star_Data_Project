00accc0fe1343fc54fffce45b4a15b89ef33b065
Anchit Lahkar <anchitlahkae0202@gmail.com>
2021-07-26 14:45:37 +0530

    7.26.2021

---
+++ main.py
@@ -0,0 +1,56 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+rows = []
+with open("Star_Data.csv", "r") as f:
+    csv_reader = csv.reader(f)
+
+    for row in csv_reader:
+        rows.append(row)
+
+
+headers = rows[0]
+star_data = rows[1:]
+
+mass = []
+radius = []
+gravity = []
+
+temp_star_data = list(star_data)
+
+for data in temp_star_data:
+    planet_gravity = data[5]
+
+    if planet_gravity.lower() == 'unknown':
+        star_data.remove(data)
+
+for data in star_data:
+    gravity.append(data[5])
+    mass.append(data[3])
+    radius.append(data[4])
+
+print('creating graph...')
+
+
+mass.sort()
+radius.sort()
+gravity.sort()
+
+
+plt.figure()
+sns.lineplot(mass, radius, marker='o')
+plt.title('Graph ( Mass, Radius)')
+plt.xlabel('Mass')
+plt.ylabel('Radius')
+plt.show()
+
+
+plt.figure()
+sns.lineplot(mass, gravity, marker='o')
+plt.title('Graph ( Mass, Gravity)')
+plt.xlabel('Mass')
+plt.ylabel('Gravity')
+plt.show()
